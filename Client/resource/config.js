7bd633443c7364f1bb0486cbba4f9e82bf9837fd
Fijon <huiqiangxu@126.com>
2019-03-17 10:36:08 +0800

    设置配置文件

---
+++ Client/resource/config.js
@@ -0,0 +1,11 @@
+var config = {
+    "dbconfig": {
+        "host": "mysql.sql122.cdncenter.net",
+        "port": 3306,
+        "user": "sq_Fijon",
+        "password": "*******",
+        "database": "sq_Fijon"
+    }
+};
+
+exports.config=config;
